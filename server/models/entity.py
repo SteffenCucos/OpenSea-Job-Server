8f178bdfda36f6e07f25dd386aaabc1672539b54
Steffen Cucos <steffen.cucos@gmail.com>
2022-07-25 18:25:26 -0400

    Add created_date field to entities

--- server/models/entity.py
+++ server/models/entity.py
@@ -1,17 +1,19 @@
+
+from datetime import datetime as Datetime
+
 from .id import Id, create_id
-
-from typing import Any
-from dataclasses import dataclass, field
-
 
 def Entity(_id_source: str = None):
     class IdEntity:
         _id: Id
+        _created_time: Datetime
 
         def __post_init__(self):
             if _id_source:
                 self._id = Id(str(getattr(self, _id_source)))
             else:
                 self._id = create_id()
+
+            self._created_time = Datetime.now()
 
     return IdEntity
