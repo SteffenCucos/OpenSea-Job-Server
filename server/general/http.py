20331cdea0d21926666e5442322ea249b5f8c687
Steffen Cucos <steffen.cucos@gmail.com>
2022-07-25 10:04:10 -0400

    Less retries

--- server/general/http.py
+++ server/general/http.py
@@ -8,10 +8,10 @@ from urllib3.util.retry import Retry
 
 def get_retry_http() -> Session:
     retry_strategy = Retry(
-        total=25,
+        total=3,
         status_forcelist=[429, 500, 502, 503, 504],
         method_whitelist=["HEAD", "GET", "OPTIONS"],
-        backoff_factor=0.01
+        backoff_factor=0.001
     )
 
     adapter = HTTPAdapter(max_retries=retry_strategy)
